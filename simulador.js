59c7ebfa10be2028c33609c5623ad3609e77d2a6
Alan-Figueredo <you@example.com>
2021-11-18 22:10:42 -0300

    cambios

--- simulador.js
+++ simulador.js
@@ -46,7 +46,6 @@ const aProductos = [
     {id: 3, nombre: "Brahma", precio: 80, descripcion:"Lata Brahma 500cc", categoria:"Cervezas",imagen: "brahma.jpg"},
     {id: 4, nombre: "Dulce de leche", precio: 120, descripcion:"Dulce de leche repostero", categoria:"Dulces", imagen: "DDL.jfif"},
 ];
-
 let productosFiltrados = aProductos;
 
 //
@@ -60,14 +59,12 @@ function filtrarDatos(){
 //Fin de filtracion de datos
 
 //ordenar datos por menor
-//let ordenarMenor = document.getElementById("ordenarMenor");
 const ordenarXMenor = ()=>{
     productosFiltrados.sort((a,b) => (a.precio > b.precio) ? 1 : ((b.precio > a.precio) ? -1 : 0));
     limpiarHTML();
     mostrarProductos();
 }
-$("#ordenarMenor").click(ordenarXMenor)
-//ordenarMenor.addEventListener("click", ordenarXMenor);
+$("#ordenarMenor").click(ordenarXMenor);
 
 //Fin ordenar datos por menor
 
@@ -79,21 +76,18 @@ const ordenarXMayor = ()=>{
     mostrarProductos();
 }
 $("#ordenarMayor").click(ordenarXMayor);
-//ordenarMayor.addEventListener("click", ordenarXMayor);
 
 //Fin ordenar datos por mayor
 
 //Limpiar estructura de productos
 const limpiarHTML = ()=>{
     let fila = document.getElementById('fila');
-    //document.getElementById("filaProductos").appendChild(fila);
     $("#fila").empty(fila);
 
     fila = document.createElement('div');
     let a = document.createAttribute("id");
     a.value = 'fila';
     fila.setAttributeNode(a);
-    //document.getElementById("filaProductos").appendChild(fila);
     $("#fila").append(fila);
 }
 
@@ -102,7 +96,9 @@ const limpiarHTML = ()=>{
 //check
 const mostrarProductos = () =>{
     for(const producto of productosFiltrados){
-        $("#fila").append(`<div class="col-12 mb-3 pr-4"><div class="card text-center"><div class="card-body"><div class="row"><div class="col-sm-2"><img src="assets/${producto.imagen}" alt="" class="img-fluid img-thumbnail" style="width: 210px;"></div><div class="col text-left"><h5 class="card-title">${producto.nombre}</h5><p class="card-text">${producto.descripcion}</p><p class="card-text"><b>$ ${producto.precio}</b></p><a href="#" class="btn btn-primary mt-4" onclick=obtenerProductosComprados(${producto.id});>Añadir </a></div></div></div></div></div>`)
+        $("#fila").append(`<div class="col-12 mb-3 pr-4"><div class="card text-center"><div class="card-body"><div class="row"><div class="col-sm-2"><img src="assets/${producto.imagen}" alt="" class="img-fluid img-thumbnail" style="width: 210px;"></div><div class="col text-left"><h5 class="card-title">${producto.nombre}</h5><p class="card-text">${producto.descripcion}</p><p class="card-text"><b>$ ${producto.precio}</b></p><a href="#" class="btn btn-primary mt-4" onclick=obtenerProductosComprados(${producto.id});>Añadir </a></div></div></div></div></div>`);
+        $("#fila").hide().fadeIn(200);
+
     }
     $("#fila").append(`<div class="col-12 mt-5 text-right pr-3 mb-5 pr-4"><a href="factura.html" class="btn btn-primary" target="_blank">Obtener Factura</a></div>`)
 }
@@ -110,7 +106,7 @@ const mostrarProductos = () =>{
 //Funcion que muestra las categorias de los productos
 const mostrarCategorias = ()=>{
     for(const elemento of aProductos){
-        $("#categorias").append(`<p class="mx-2">${elemento.categoria}</p><br>`)
+        $("#categorias").append(`<p class="mx-2">${elemento.categoria}</p><br>`);
     }
 }
 
@@ -127,9 +123,7 @@ const filtrarMaxMin=()=>{
     }
     $("#fila").append(`<div class="col-12 mt-5 text-right pr-3 mb-5 pr-4"><a href="factura.html" class="btn btn-primary" target="_blank">Obtener Factura</a></div>`)
 }
-$("#enviarMaxMin").click(filtrarMaxMin)
-//let enviarMaxMin = document.getElementById("enviarMaxMin");
-//enviarMaxMin.addEventListener("click", filtrarMaxMin);
+$("#enviarMaxMin").click(filtrarMaxMin);
 
 //Fin funcion para filtrar por maximo y minimo
 
