791f1e5956727685478dbe5426529f9cfe87ab00
Alan Figueredo <afigueredo2000@gmail.com>
2021-12-02 13:27:14 -0300

    cambios

--- carrito.js
+++ carrito.js
@@ -7,7 +7,7 @@ const verFactura = () =>{
         }else{
             fact.push(prod.categoria);
         }
-        $("#fila").append(`<div class="row mb-5"><div class="col"><p>${prod.nombre}</p></div><div class="col text-right"> <i class="fas fa-minus-square" id="restar${prod.id}"></i> <input type="text" placeholder="${prod.cantidad}" style="width: 50px;height: 34px;padding-bottom: 6px;"><button class="btn btn-primary ml-2" id="sumar${prod.id}">+</button></div></div>`);
+        $("#fila").append(`<div class="row mb-5"><div class="col"><p>${prod.nombre}</p></div><div class="col text-right"> <i class="fas fa-minus-square" id="restar${prod.id}"></i> <input type="text" placeholder="${prod.cantidad}" style="width: 50px;height: 34px;padding-bottom: 6px;"> <i class="fas fa-plus-square" id="sumar${prod.id}"></i> </div></div>`);
         $(`#restar${prod.id}`).click(function(){
             prod.cantidad-=1;
             console.log(prod.cantidad)
